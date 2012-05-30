5936052f20a4038c227508753b1b6e7cb414e05a
Richard Olsson <r@richardolsson.se>
2012-05-30 12:30:31 +0200

    Optimized generation of view projection matrix through caching/invalidation.

--- src/core/Camera3D.js
+++ src/core/Camera3D.js
@@ -7,23 +7,43 @@ function()
 {
     var Camera3D = function(lens)
     {
+        away3d.Object3D.call(this);
+
         this.lens = lens || new away3d.PerspectiveLens();
+
+        this.$.viewProjection =  new away3d.Matrix3D();
+        this.$.viewProjectionDirty = true;
     };
 
     Camera3D.prototype = new away3d.Object3D();
     Camera3D.prototype.constructor = Camera3D;
 
 
+    Camera3D.prototype.invalidateSceneTransform = function()
+    {
+        away3d.Object3D.prototype.invalidateSceneTransform.call(this);
+
+        this.invalidateViewProjection();
+    };
+
+
+    Camera3D.prototype.invalidateViewProjection = function()
+    {
+        this.$.viewProjectionDirty = true;
+    };
+
+
     Camera3D.prototype.getViewProjection = function(aspectRatio)
     {
-        // TODO: Optimize this (cache and validate)
-        var vp = new away3d.Matrix3D();
-        var inv = new away3d.Matrix3D();
+        if (this.$.viewProjectionDirty) {
+            // TODO: inverse transform should be on Object3D
+            var inv = new away3d.Matrix3D();
+            inv.inv(this.transform);
+            this.$.viewProjection.mul(inv, this.lens.getProjectionMatrix(aspectRatio));
+            this.$.viewProjectionDirty = false;
+        }
 
-        inv.inv(this.transform);
-        vp.mul(inv, this.lens.getProjectionMatrix(aspectRatio));
-
-        return vp;
+        return this.$.viewProjection;
     };
 
 
