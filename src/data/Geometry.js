e5d75e49412eee6604e7a81ea6318417c531b794
Richard Olsson <r@richardolsson.se>
2012-06-01 16:23:00 +0200

    Made geometry buffer objects "private".

--- src/data/Geometry.js
+++ src/data/Geometry.js
@@ -7,44 +7,50 @@ function()
         this.indices = [];
         this.colors = [];
 
-        this.vertexBuffer = null;
-        this.indexBuffer = null;
-        this.colorBuffer = null;
+        this.$ = {
+            vertexBuffer: null,
+            indexBuffer: null,
+            colorBuffer: null,
+
+            vertexBufferDirty: true,
+            indexBufferDirty: true,
+            colorBufferDirty: true
+        };
     };
 
     Geometry.prototype.getVertexBuffer = function(gl)
     {
-        if (!this.vertexBuffer) {
-            this.vertexBuffer = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        if (this.$.vertexBufferDirty) {
+            this.$.vertexBuffer = this.$.vertexBuffer || gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.$.vertexBuffer);
             gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
         }
 
-        return this.vertexBuffer;
+        return this.$.vertexBuffer;
     };
 
 
     Geometry.prototype.getIndexBuffer = function(gl)
     {
-        if (!this.indexBuffer) {
-            this.indexBuffer = gl.createBuffer();
-            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+        if (this.$.indexBufferDirty) {
+            this.$.indexBuffer = this.$.indexBuffer || gl.createBuffer();
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.$.indexBuffer);
             gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.indices), gl.STATIC_DRAW);
         }
 
-        return this.indexBuffer;
+        return this.$.indexBuffer;
     };
 
 
     Geometry.prototype.getColorBuffer = function(gl)
     {
-        if (!this.colorBuffer) {
-            this.colorBuffer = gl.createBuffer();
-            gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
+        if (this.$.colorBufferDirty) {
+            this.$.colorBuffer = this.$.colorBuffer || gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.$.colorBuffer);
             gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.colors), gl.STATIC_DRAW);
         }
 
-        return this.colorBuffer;
+        return this.$.colorBuffer;
     };
 
     return Geometry;
